4395c57365ddced9379b22031eecc63f52b6cdbf
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-04-17 14:15:07 +0300

    Release 0.2.1.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (
 
 setup(
     name='collective.behavior.size',
-    version='0.2.1.2',
+    version='0.2.1.3',
     description="Provides size related behavior, such as weight and three dimensional size, to dexterity content types.",
     long_description=long_description,
     classifiers=[
